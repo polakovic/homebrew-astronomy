10a468def5c44702090cde8fc2020efafc579dde
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2013-11-17 23:08:39 +0100

    QHY driver added

---
+++ indi-qhy.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class IndiSx < Formula
+  homepage 'http://www.indilib.org'
+  url 'http://svn.code.sf.net/p/indi/code/trunk/3rdparty/indi-qhy', :using => :svn, :revision => '959'
+  head 'http://svn.code.sf.net/p/indi/code/trunk/3rdparty/indi-qhy', :using => :svn
+  version "0.9.6"
+
+  depends_on 'cmake' => :build
+  depends_on 'polakovic/astronomy/libindi'
+  depends_on 'libusb'
+  depends_on 'libusb-compat'
+  depends_on 'cfitsio'
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
