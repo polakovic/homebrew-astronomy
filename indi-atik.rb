c3c41c693bee88537780e138a3da461b99dec601
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2014-01-14 19:49:59 +0100

    FS series support added

--- indi-atik.rb
+++ indi-atik.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class IndiAtik < Formula
   homepage 'http://www.indilib.org'
-  url 'http://download.cloudmakers.eu/indi-atik-0.6.tar.gz'
-  sha1 '447d28f3d6448987895bf6a5919d02141a623698'
+  url 'http://download.cloudmakers.eu/indi-atik-0.7.tar.gz'
+  sha1 'a424687f5a52c8e6ffa6c2987dff60c24a7b3657'
 
   depends_on 'cmake' => :build
   depends_on 'polakovic/astronomy/libindi'
