367648db0333f9061cd87505edfec25a95525b95
Peter Polakovic <peter.polakovic@gmail.com>
2014-01-30 18:43:19 +0100

    objects instead of executables in archive

--- indi-atik.rb
+++ indi-atik.rb
@@ -3,13 +3,13 @@ require 'formula'
 class IndiAtik < Formula
   homepage 'http://www.indilib.org'
   url 'http://download.cloudmakers.eu/indi-atik-0.9.tar.gz'
-  sha1 '2abbd6bf0540b7092e73fc72c7de57f259c08083'
+  sha1 '6c01ed75ae89c3f282b3883681f132a1ee6be1b2'
 
   depends_on 'cmake' => :build
   depends_on 'polakovic/astronomy/libindi'
 
   def install
-    bin.install Dir['bin/*']
-    share.install Dir['share/*']
+    system "cmake", ".", "-DBREW=1", *std_cmake_args
+    system "make", "install"
   end
 end
