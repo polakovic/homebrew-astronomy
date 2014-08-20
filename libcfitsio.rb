d806d91a0cedbf8bfa8bf9da27b172ba08527f90
Peter Polakovic <polakovic@Retina.local>
2014-08-20 19:07:17 +0200

    Added libcfitsio with correct SHA1

---
+++ libcfitsio.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+class Libcfitsio < Formula
+  homepage "http://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html"
+  url "http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz"
+  sha1 "48bd6389dcff3228508eec70384f2cae3a88ff32"
+  version "3.370"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make shared"
+    system "make install"
+
+    if build.with? "examples"
+      system "make fpack funpack"
+      bin.install "fpack", "funpack"
+
+      resource("examples").stage do
+        # compressed_fits.c does not work (obsolete function call)
+        (Dir["*.c"] - ["compress_fits.c"]).each do |f|
+          system ENV.cc, f, "-I#{include}", "-L#{lib}", "-lcfitsio", "-lm", "-o", "#{bin}/#{f.sub('.c', '')}"
+        end
+      end
+    end
+  end
+end
