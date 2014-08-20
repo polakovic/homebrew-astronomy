dbd149cc2715f28821562820165edf8dc35148a3
Peter Polakovic <polakovic@Retina.local>
2014-08-20 23:09:27 +0200

    indi-atik 0.14

--- indi-atik.rb
+++ indi-atik.rb
@@ -2,14 +2,19 @@ require 'formula'
 
 class IndiAtik < Formula
   homepage 'http://www.indilib.org'
-  url 'http://download.cloudmakers.eu/indi-atik-0.9.tar.gz'
-  sha1 'c75e976e022282af542df4c3fe97bc7dc344136c'
+  url 'http://download.cloudmakers.eu/indi-atik-0.14.tar.gz'
+  sha1 'fe85f550a3fdb3c195b55dd88c8fbd333f59de1b'
 
   depends_on 'cmake' => :build
   depends_on 'polakovic/astronomy/libindi'
 
   def install
-    system "cmake", ".", "-DBREW=1", *std_cmake_args
-    system "make", "install"
+    prefix.install "COPYRIGHT.txt"
+    bin.install Dir["bin/*"]
+    lib.install Dir["lib/*"]
+    share.install Dir["share/*"]
+    system "install_name_tool -id /usr/local/libatikccd.0.dylib #{prefix}/lib/libatikccd.0.dylib"
+    system "install_name_tool -change /Users/polakovic/Development/indi-atik/libatikccd.0.dylib /usr/local/lib/libatikccd.0.dylib #{prefix}/bin/indi_atik_ccd"
+    system "install_name_tool -change /Users/polakovic/Development/indi-atik/libatikccd.0.dylib /usr/local/lib/libatikccd.0.dylib #{prefix}/bin/atik_ccd_test"
   end
 end
