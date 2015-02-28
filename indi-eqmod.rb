fc66e6c72400fcd174c8484be3e49b4f931eec39
Peter Polakovic <polakovic@Retina.local>
2015-02-28 19:42:52 +0100

    Fixed EQMod driver

--- indi-eqmod.rb
+++ indi-eqmod.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class IndiEqmod < Formula
   homepage 'http://www.indilib.org'
-  url 'http://svn.code.sf.net/p/indi/code/trunk/3rdparty/indi-eqmod', :using => :svn, :revision => '2030'
+  url 'http://svn.code.sf.net/p/indi/code/trunk/3rdparty/indi-eqmod', :using => :svn, :revision => '2057'
   head 'http://svn.code.sf.net/p/indi/code/trunk/3rdparty/indi-eqmod', :using => :svn 
-  version "1.0.0.2030"
+  version "1.0.0.2057"
 
   depends_on 'cmake' => :build
   depends_on 'polakovic/astronomy/libindi'
