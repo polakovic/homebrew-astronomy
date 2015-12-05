570de132c2965e9ebda1c849c6a3b504afaa3557
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2015-12-05 21:18:50 +0100

    Update to INDI 1.1 r2578

--- indi-aagcloudwatcher.rb
+++ indi-aagcloudwatcher.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class IndiAagcloudwatcher < Formula
   homepage 'http://www.indilib.org'
-  url 'http://svn.code.sf.net/p/indi/code/trunk/3rdparty/indi-aagcloudwatcher', :using => :svn, :revision => '2439'
+  url 'http://svn.code.sf.net/p/indi/code/trunk/3rdparty/indi-aagcloudwatcher', :using => :svn, :revision => '2578'
   head 'http://svn.code.sf.net/p/indi/code/trunk/3rdparty/indi-aagcloudwatcher', :using => :svn
-  version "1.1.0.2439"
+  version "1.1.0.2578"
 
   depends_on 'cmake' => :build
   depends_on 'polakovic/astronomy/libindi'
